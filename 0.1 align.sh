562df788e186d0c0a29da0d8f3503fce796535db
RuBP <maningxin824@gmail.com>
2017-07-17 14:56:44 -0400

    run hisat2 aligner

---
+++ 0.1 align.sh
@@ -0,0 +1,31 @@
+# Exit this script on any error.
+set -euo pipefail
+
+# This keeps track of the messages printed during execution.
+RUNLOG=runlog.txt
+echo "Run by `whoami` on `date`" > $RUNLOG
+
+# Create output folder
+mkdir -p bam
+
+# The index determines what the data is aligned against.
+IDX=~/anno/GRCm38.primary_assembly.genome
+
+for SAMPLE in S14 S14G S14N S27G S27N W
+do
+	for REP in 1 2 3;
+	do
+		# Build the name of the files
+		R1=reads/${SAMPLE}-${REP}.fastq.gz
+		BAM=bam/${SAMPLE}-${REP}.bam
+
+		# Run the aligner
+		echo '*** Aligning: $BAM'
+		hisat2 $IDX -U $R1 2>> $RUNLOG | samtools sort > $BAM 2>> $RUNLOG
+		samtools index $BAM
+	done
+done
+
+hisat2 $IDX -U reads/S27.fastq.gz 2>> $RUNLOG | samtools sort > bam/S27.bam 2>> $RUNLOG
+
+hisat2 $IDX -U reads/S27G-4.fastq.gz 2>> $RUNLOG | samtools sort > bam/S27G-4.bam 2>> $RUNLOG
