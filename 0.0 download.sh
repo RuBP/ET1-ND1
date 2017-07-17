022f491ce296a866c0a5d4e938071b7e5ce16e57
RuBP <maningxin824@gmail.com>
2017-07-17 13:56:07 -0400

    download genome and build index

--- 0.0 download.sh
+++ 0.0 download.sh
@@ -1,8 +1,12 @@
 # 2017/07/11 https://www.gencodegenes.org/mouse_releases/current.html
+# Git repo created following http://swcarpentry.github.io/git-novice/03-create/
 
 # Download mouse(M14) genome sequence, primary assembly (GRCm38) from Gencode
 curl -O ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_mouse/release_M14/GRCm38.primary_assembly.genome.fa.gz
 gunzip GRCm38.primary_assembly.genome.fa.gz 
+
+# Build Hisa2 index for mouse genome GRCm38
+hisat2-build GRCm38.primary_assembly.genome.fa GRCm38.primary_assembly.genome
 
 # Download comprehensive gene annotation GTF (regions CHR)
 curl -O ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_mouse/release_M14/gencode.vM14.annotation.gtf.gz
